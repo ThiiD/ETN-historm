a32c00e92db82c97beb96388a0b799668790f7e3
João Alex <97920941+joaoalexarruda@users.noreply.github.com>
2024-05-16 10:24:15 -0300

    checkpoint
    filter is working

--- pages/1_HI82.py
+++ pages/1_HI82.py
@@ -12,8 +12,16 @@ def HI82():
     if file:
         df = pd.read_excel(file)
         df = df.loc[(df['Holtec/Casks/HI82/Average Temp'] > 0)]
+        # Let the user select the minimum value
+        min_val = st.number_input(
+            'Enter the minimum value for Holtec/Environment/TIA/Value', value=df['Holtec/Environment/TIA/Value'].min())
+        # Let the user select the maximum value
+        max_val = st.number_input(
+            'Enter the maximum value for Holtec/Environment/TIA/Value', value=df['Holtec/Environment/TIA/Value'].max())
+        df = df[(df['Holtec/Environment/TIA/Value'] >= min_val) & (
+            df['Holtec/Environment/TIA/Value'] <= max_val)]  # Filter DataFrame
         fig = px.scatter(df, x='t_stamp', y=[
-                         'Holtec/Casks/HI82/Average Temp'], title='Temperature Data')
+                         'Holtec/Casks/HI82/Average Temp'], title='HI82 Temperature Data')
         # Set y-axis limits and title
         fig.update_yaxes(range=[0, 100], title_text='Temperature (°C)')
         fig.update_xaxes(title_text='Date - Time')  # Set x-axis title
