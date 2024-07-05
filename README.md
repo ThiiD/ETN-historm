854265846480e0fcab8b77ed6914adc2484e263a
João Alex <97920941+joaoalexarruda@users.noreply.github.com>
2024-07-05 13:06:06 -0300

    update readme

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
-# HS
+# ETN HI-STORM Temperature Data Plotter
 
 This is a simple automation script to plot temperature data from equipment.
 
 ## Streamlit
 
+The app is hosted on Streamlit sharing, you can access it by clicking the badge below:
+
 [![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://etn-hs-temp.streamlit.app/)
+
+## Usage
+
+After getting the data from the equipment, just open the Streamlit app and upload the file.
+
+It will plot the data and show the temperature values, allowing you to download all the plots as PNG files. It also generates the WORD document to be used in the report.
+
+## Code Style
+
+This project adheres to the Python code style standards using Black. You can ensure your code contributions are properly formatted by using Black.
+
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
--- pages/1_Single_HISTORM.py
+++ pages/1_Single_HISTORM.py
@@ -5,20 +5,45 @@ from io import BytesIO
 
 
 def SingleHISTORM():
+    """
+    Streamlit app for visualizing and analyzing temperature data for a single HISTORM unit.
+
+    Allows the user to upload an Excel file with temperature data for a specific HISTORM unit (62, 72, or 82),
+    visualize the data through a scatter plot, and download the filtered dataset based on user-defined temperature
+    range.
+    """
+    # Set Streamlit page configuration
     st.set_page_config(
         page_title="Single HISTORM Temperature Data",
         page_icon=":bar_chart:",
         layout="centered",
     )
-    st.title("Single HISTORM Temperature Data (62, 72, 82)")
 
-    # Create a dropdown menu and store the selected option in a variable
-    option = st.selectbox("Choose an option", ("62", "72", "82"))
+    # Display the app title
+    st.title("Single HISTORM Temperature Data (42, 52, 62, 72, 82, 63, 73, 83)")
 
+    # Dropdown menu for selecting the HISTORM unit
+    option = st.selectbox(
+        "Choose an option",
+        (
+            "42",
+            "52",
+            "62",
+            "72",
+            "82",
+            "63",
+            "73",
+            "83",
+        ),
+    )
+
+    # File uploader for the Excel file containing the temperature data
     file = st.file_uploader(f"Upload xlsx file with HI{option} data", type=["xlsx"])
 
     if file:
+        # Read the Excel file into a DataFrame
         df = pd.read_excel(file)
+        # Filter the DataFrame based on temperature conditions
         df = df.loc[
             (df[f"Holtec/Casks/HI{option}/Average Temp"] > 0)
             & (df[f"Holtec/Casks/HI{option}/Average Temp"] < 200)
@@ -28,20 +53,23 @@ def SingleHISTORM():
             & (df[f"Holtec/Casks/HI{option}/Delta Temp"] > 0)
         ]
 
-        # Let the user select the minimum value
+        # Number input for setting the minimum environment temperature
         min_val = st.number_input(
             "Mininum Environment Temperature (°C)",
             value=df["Holtec/Environment/TIA/Value"].min(),
         )
-        # Let the user select the maximum value
+        # Number input for setting the maximum environment temperature
         max_val = st.number_input(
             "Maximum Environment Temperature (°C)",
             value=df["Holtec/Environment/TIA/Value"].max(),
         )
+        # Further filter the DataFrame based on the user-defined temperature range
         df = df[
             (df["Holtec/Environment/TIA/Value"] >= min_val)
             & (df["Holtec/Environment/TIA/Value"] <= max_val)
-        ]  # Filter DataFrame
+        ]
+
+        # Generate a scatter plot of the temperature data
         fig = px.scatter(
             df,
             x="t_stamp",
@@ -52,16 +80,17 @@ def SingleHISTORM():
             ],
             title=f"HI{option} Temperature Data",
         )
-        # Set y-axis limits and title
+        # Update plot axes and titles
         fig.update_yaxes(range=[0, 100], title_text="Temperature (°C)")
-        fig.update_xaxes(title_text="Date - Time")  # Set x-axis title
+        fig.update_xaxes(title_text="Date - Time")
+        # Display the plot in the Streamlit app
         st.plotly_chart(fig)
 
-        # Create a BytesIO object and write the DataFrame to it using ExcelWriter
+        # Prepare the DataFrame for download
         output = BytesIO()
         with pd.ExcelWriter(output, engine="xlsxwriter") as writer:
             df.to_excel(writer, index=False)
-        output.seek(0)  # Go to the start of the BytesIO object
+        output.seek(0)  # Reset the pointer to the beginning of the stream
 
         # Create a download button for the Excel file
         st.download_button(
@@ -72,4 +101,5 @@ def SingleHISTORM():
         )
 
 
+# Run the Streamlit app
 SingleHISTORM()
